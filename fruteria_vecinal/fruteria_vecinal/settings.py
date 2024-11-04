e49ef9a607aa106548fe9d7ab3b7250ba421fdb2
Chacarerin <rubenschnettlerl@gmail.com>
2024-11-04 17:25:57 -0300

    database

--- fruteria_vecinal/fruteria_vecinal/settings.py
+++ fruteria_vecinal/fruteria_vecinal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'fruteria_vecinal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fruteria_vecinal',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
